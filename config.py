d30bd85820f715e962584d8322c211b4d4cfb1f5
Dmitriy Kondyrev <dkondyrev@gmail.com>
2019-04-03 16:11:35 +0700

    Change user rules reading

---
+++ config.py
@@ -0,0 +1,90 @@
+import yaml
+
+
+class ConfigurationError(Exception):
+    pass
+
+
+def validators():
+    registry = {}
+
+    def reg(func):
+        registry[func.__name__] = func
+        return func
+    reg.all = registry
+    return reg
+
+
+validator = validators()
+rule_validator = validators()
+
+
+def parse_config(path):
+    with open(path, 'r') as f:
+        return yaml.safe_load(f)
+
+
+def validate(config):
+    errors = []
+    for _, v in validator.all.items():
+        errors.append(v(config))
+    return filter(lambda x: x is not None, errors)
+
+
+def check_required(value, name):
+    if value is None:
+        raise ConfigurationError(
+            f'{name} in configuration file is None, please specify {name}')
+
+
+def check_type(value, v_type, name):
+    if not isinstance(value, v_type):
+        raise ConfigurationError(
+            f'{name} in configuration file is not {v_type}, '
+            f'type: {type(value)}')
+
+
+@rule_validator
+def check_index(config):
+    index = config.get('index')
+
+    try:
+        check_required(index, 'index')
+        check_type(index, str, 'index')
+    except ConfigurationError as e:
+        return e
+
+    if config.get('index') == '':
+        return ConfigurationError(
+            'index in configuration is empty string, please specify index')
+
+
+@rule_validator
+def check_rule_type(config):
+    correct_types = {'any', 'blacklist', 'whitelist', 'change', 'frequency', 'spike',
+                     'flatline', 'new_term', 'cardinality', 'metric_aggregation', 'percentage_match'}
+    rule_type = config.get('type')
+
+    try:
+        check_required(rule_type, 'type')
+        check_type(rule_type, str, 'type')
+    except ConfigurationError as e:
+        return e
+
+    if rule_type not in correct_types:
+        return ConfigurationError(
+            f'Elastalert rule type {rule_type} in configuration is incorrect.'
+            f'Type must be one of {", ".join(correct_types)}.')
+
+
+def validate_user_rule(user_rule):
+    errors = []
+    for _, v in rule_validator.all.items():
+        errors.append(v(user_rule))
+    return filter(lambda x: x is not None, errors)
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,53 +1,35 @@
+import os
 from kubernetes import config
 import kubernetes.client
 import yaml
 import logging
-from jsonschema import validate, ValidationError
+from config import validate_user_rule
 
-LOGGER = logging.getLogger()
-
-
-class IncorrectConfigException(Exception):
-    def __init__(self, message):
-        self.message = message
-
-
-def check_user_config(user_config):
-    with open('./schemas/user_config_schema.yaml', 'r') as schema_file:
-        user_config_schema = yaml.load(schema_file)
-
-    validate(user_config, user_config_schema)
-
-
-def read_user_config(config_file):
-    """
-    Read user elastalert configuration
-    :param config_file: configuration file
-    :return: user elastalert configuration
-    """
-    user_config = yaml.load(config_file)
-    check_user_config(user_config)
-
-    return user_config
+logging.basicConfig(level=os.environ.get('LOG_LEVEL', logging.INFO))
+log = logging.getLogger(__name__)
 
 
 def read_config_files(configuration):
     """
     Read configuration files from configmaps in all available namespaces
     :param configuration: Kubernetes client configuration
-    :return: list of user elastalert configurations
+    :return: list of user elastalert rules
     """
     api_instance = kubernetes.client.CoreV1Api(kubernetes.client.ApiClient(configuration))
     configmap_list = api_instance.list_config_map_for_all_namespaces()
 
     user_configs = []
     for configmap in configmap_list:
-        try:
-            user_config = read_user_config(configmap.data)
-            user_configs.append(user_config)
-        except ValidationError as e:
-            LOGGER.exception(e.message)
-            continue
+        if configmap.metadata.name == 'elastalert-rules':
+            for rule_name in configmap.data:
+                user_rule = yaml.safe_load(configmap.data[rule_name])
+                errors = validate_user_rule(user_rule)
+                if len(errors) != 0:
+                    for error in errors:
+                        log.error(error)
+                    continue
+
+                user_configs.append(user_rule)
 
     return user_configs
 

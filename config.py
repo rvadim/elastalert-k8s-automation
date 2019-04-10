004ba78ae82fd07e3114fdd4a6a39d50fafb1b86
Dmitriy Kondyrev <dkondyrev@gmail.com>
2019-04-10 13:40:51 +0700

    Read configuration from configmaps in user namespaces (#9)
    * Initial project
    
    * Dockerfile changes
    
    * Read configuration from configmaps in user namespaces
    
    * Revert "Read configuration from configmaps in user namespaces"
    
    This reverts commit 55bdcb60dd349190b91e6139f9bd07a1615f67c0.
    
    * Read configuration from configmaps in user namespaces
    
    * Change user rules reading
    
    * Add field_selector to config map query
    
    * Remove configuration schema

--- config.py
+++ config.py
@@ -16,6 +16,7 @@ def validators():
 
 
 validator = validators()
+rule_validator = validators()
 
 
 def parse_config(path):
@@ -28,7 +29,6 @@ def validate(config):
     for _, v in validator.all.items():
         errors.append(v(config))
     return filter(lambda x: x is not None, errors)
-
 
 @validator
 def check_es_host(config):
@@ -57,3 +57,56 @@ def check_es_port(config):
         return ConfigurationError(
             'es_port in configuration file is not in range({}), '
             'please specify es_port in range 1 < port < 65535'.format(port))
+  
+  
+def check_required(value, name):
+    if value is None:
+        raise ConfigurationError(
+            f'{name} in configuration file is None, please specify {name}')
+
+
+def check_type(value, v_type, name):
+    if not isinstance(value, v_type):
+        raise ConfigurationError(
+            f'{name} in configuration file is not {v_type}, '
+            f'type: {type(value)}')
+
+
+@rule_validator
+def check_index(config):
+    index = config.get('index')
+
+    try:
+        check_required(index, 'index')
+        check_type(index, str, 'index')
+    except ConfigurationError as e:
+        return e
+
+    if config.get('index') == '':
+        return ConfigurationError(
+            'index in configuration is empty string, please specify index')
+
+
+@rule_validator
+def check_rule_type(config):
+    correct_types = {'any', 'blacklist', 'whitelist', 'change', 'frequency', 'spike',
+                     'flatline', 'new_term', 'cardinality', 'metric_aggregation', 'percentage_match'}
+    rule_type = config.get('type')
+
+    try:
+        check_required(rule_type, 'type')
+        check_type(rule_type, str, 'type')
+    except ConfigurationError as e:
+        return e
+
+    if rule_type not in correct_types:
+        return ConfigurationError(
+            f'Elastalert rule type {rule_type} in configuration is incorrect.'
+            f'Type must be one of {", ".join(correct_types)}.')
+
+
+def validate_user_rule(user_rule):
+    errors = []
+    for _, v in rule_validator.all.items():
+        errors.append(v(user_rule))
+    return filter(lambda x: x is not None, errors)
--- main.py
+++ main.py
@@ -1,12 +1,43 @@
 import os
 import sys
+
+from kubernetes import config
+import kubernetes.client
+import yaml
+import logging
 from config import parse_config
 from config import validate
-from kubernetes import config
+from config import validate_user_rule
 
-import logging
 logging.basicConfig(level=os.environ.get('LOG_LEVEL', logging.INFO))
 log = logging.getLogger(__name__)
+
+
+def read_config_files(configuration):
+    """
+    Read configuration files from configmaps in all available namespaces
+    :param configuration: Kubernetes client configuration
+    :return: list of user elastalert rules
+    """
+    api_instance = kubernetes.client.CoreV1Api(kubernetes.client.ApiClient(configuration))
+    configmap_list = api_instance.list_config_map_for_all_namespaces(field_selector='metadata.name=elastalert-rules')
+
+    user_configs = []
+    for configmap in configmap_list:
+        for rule_name in configmap.data:
+            try:
+                user_rule = yaml.safe_load(configmap.data[rule_name])
+                errors = validate_user_rule(user_rule)
+                if len(errors) != 0:
+                    for error in errors:
+                        log.error(error)
+                    continue
+
+                user_configs.append(user_rule)
+            except yaml.YAMLError as e:
+                log.error(f'Failed to parse configuration. {e}')
+
+    return user_configs
 
 
 def main():
@@ -17,7 +48,10 @@ def main():
         for error in errors:
             log.error(error)
         sys.exit(1)
+
     config.load_incluster_config()
+    configuration = kubernetes.client.Configuration()
+    user_configs = read_config_files(configuration)
 
 
 if __name__ == "__main__":

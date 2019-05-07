ea157f9147465be9627d99a65a94239055ed341b
Vladimir Kuyantsev <vkujantsev94@gmail.com>
2019-05-07 14:04:21 +0700

    test remade using Renderer class

--- tests/test_jinja_custom_filter.py
+++ tests/test_jinja_custom_filter.py
@@ -1,12 +1,10 @@
-from config_generator import yaml_filter
-from jinja2 import Environment, FileSystemLoader
+from config_generator import Renderer
 import yaml
 
 
 class TestJinjaCustomFilter:
     def test_yaml_filter(self):
-        env = Environment(loader=FileSystemLoader('./tests/fixtures/test_jinja'))
-        env.filters['toyaml'] = yaml_filter
-        template = env.get_template('test_yaml_filter_template.j2')
+        renderer = Renderer('./tests/fixtures/test_jinja')
         parsed_file = yaml.safe_load(open('./tests/fixtures/test_jinja/file_for_render'))
-        assert parsed_file == yaml.safe_load(template.render(parsed_file))
+        generated = renderer._generate_config('test_yaml_filter_template.j2', parsed_file)
+        assert parsed_file == yaml.safe_load(generated)
